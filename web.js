6eb4db3b5ed23698e40ad306773b61d886245107
Pedro R. Jimenez <countzyx@gmail.com>
2013-07-11 06:18:52 +0000

    Updated web.js to render text.
    Moved the fs.readFileSync to the / get function. Hopefully this works.

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 
-var sIndexFileName = 'index.html';
-var sData = fs.readFileSync(sIndexFileName);
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var sData = fs.readFileSync('./index.html', 'utf8');
   response.send(sData);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
