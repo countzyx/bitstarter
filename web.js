f6e31d23d67af815699fb1514dea090fbf384088
Pedro R. Jimenez <countzyx@gmail.com>
2013-07-11 06:13:09 +0000

    Modified web.js To Read index.html
    First attempt. I hope it works.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var sIndexFileName = 'index.html';
+var sData = fs.readFileSync(sIndexFileName);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(sData);
 });
 
 var port = process.env.PORT || 5000;
